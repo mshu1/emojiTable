0b500abf897e1a1239de32cd462ab8150184a89a
Michelle <mshu1@jhu.edu>
2017-03-21 01:31:02 -0400

    finish~

--- emojiTable/ViewController.swift
+++ emojiTable/ViewController.swift
@@ -8,17 +8,105 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
+
+
+    
+    @IBOutlet weak var trayView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        trayDownOffset = 148
+        trayUp = trayView.center
+        trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        let velocity = sender.velocity(in: view)
+
+        if sender.state == .began {
+            trayOriginalCenter = trayView.center
+        } else if sender.state == .changed {
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            if velocity.y > 0 {
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayDown
+                }, completion: nil)
+            } else {
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayUp
+                }, completion: nil)
+            }
+        }
+
+    }
+    
+    @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+        //let location = sender.location(in: view)
+        //let velocity = sender.velocity(in: view)
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            let imageView = sender.view as! UIImageView
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            view.addSubview(newlyCreatedFace)
+            newlyCreatedFace.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            newlyCreatedFace.center = imageView.center
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+        } else if sender.state == .changed {
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+
+        } else if sender.state == .ended {
+            let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.handlePan(sender:)))
+            
+            gestureRecognizer.delegate = self
+            newlyCreatedFace.addGestureRecognizer(gestureRecognizer)
+            
+            newlyCreatedFace.isUserInteractionEnabled = true
+            newlyCreatedFace.transform = newlyCreatedFace.transform.scaledBy(x: (2/3), y: (2/3))
+
+            print("Gesture ended")
+        }
+        
+    }
+    
+    func handlePan(sender: UIPanGestureRecognizer) {
+        //let location = sender.location(in: view)
+        //let velocity = sender.velocity(in: view)
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            newlyCreatedFace = sender.view as! UIImageView
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+            newlyCreatedFace.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        } else if sender.state == .changed {
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            newlyCreatedFace.transform = newlyCreatedFace.transform.scaledBy(x: (2/3), y: (2/3))
+            print("Gesture ended")
+        }
+    }
+    
+
 
 
 }
